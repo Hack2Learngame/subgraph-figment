22c9690a7d40e92aca31e311033987c8922655fd
maexx393 <103386357+monyverse@users.noreply.github.com>
2022-05-22 13:48:35 -0400

    Initial commit

---
+++ truffle.js
@@ -0,0 +1,36 @@
+require('babel-register')
+require('babel-polyfill')
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+    },
+    aurora: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://testnet.aurora.dev/${process.env.AURORA_API_KEY}`
+        )
+      },
+      network_id: '1313161555',
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_API_KEY}`
+        )
+      },
+      network_id: '3',
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.4.25'    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+}
